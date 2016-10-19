6cd2c4717bf19f95e08ad0c5d92f5a151acd329f
Adithya Narayan <24adithya@gmail.com>
2016-10-19 23:20:00 +0530

    dynamic point cut example

---
+++ src/main/java/org/pack/ch5/SampleBean.java
@@ -0,0 +1,11 @@
+package org.pack.ch5;
+
+public class SampleBean {
+	public void foo(int x) {
+		System.out.println("Invoked foo() with: " + x);
+	}
+
+	public void bar() {
+		System.out.println("Invoked bar()");
+	}
+}
---
+++ src/main/java/org/pack/ch5/SimpleAdvice.java
@@ -0,0 +1,13 @@
+package org.pack.ch5;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class SimpleAdvice implements MethodInterceptor {
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		System.out.println(">>Invoking " + invocation.getMethod().getName());
+		Object retVal = invocation.proceed();
+		System.out.println(">>Done");
+		return retVal;
+	}
+}
