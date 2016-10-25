15ee87ff5d47e0084482c4febbba32488b429014
Adithya Narayan <24adithya@gmail.com>
2016-10-26 00:17:35 +0530

    ch5 - introductions

---
+++ src/main/java/org/pack/ch5/TargetBean.java
@@ -0,0 +1,13 @@
+package org.pack.ch5;
+
+public class TargetBean {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
